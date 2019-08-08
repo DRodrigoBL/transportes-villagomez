991c0f1fd0a42de3d781265abbf65c1653cc0693
Rodrigo Bautista <rodbautista711@gmail.com>
2019-08-07 22:03:09 -0500

    ngrx configured in project for ui-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { StoreModule } from '@ngrx/store';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 
+// modules
+import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
-import { HeaderComponent } from './navigation/header/header.component';
-import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { AuthModule } from './auth/auth.module';
 
-import { OAuthGoogleService } from './auth/services/google.service';
-import { MAT_DATE_LOCALE } from '@angular/material';
+// components
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+
+// services
+import { OAuthGoogleService } from './auth/services/google.service';
+import { UIService } from './shared/ui.service';
+
+// ngrx
+import { reducers } from './app.reducer';
+
+// other
+import { MAT_DATE_LOCALE } from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -27,16 +39,18 @@ import { WelcomeComponent } from './welcome/welcome.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MaterialModule,
     FlexLayoutModule,
     AngularFireModule.initializeApp(environment.firebase),
     AuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    StoreModule.forRoot(reducers)
   ],
   providers: [
     OAuthGoogleService,
+    UIService,
     { provide: MAT_DATE_LOCALE, useValue: 'es-us' }
   ],
   bootstrap: [AppComponent]
