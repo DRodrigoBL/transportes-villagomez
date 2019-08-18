0103a76f0beb457de938357eeac546c75719d571
Rodrigo Bautista <rodbautista711@gmail.com>
2019-08-18 14:39:56 -0500

    prod env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD-FL1S-F7v-r-lUS7ZnDpJPAqrTQ1dW5A',
+    authDomain: 'transportes-villagomez.firebaseapp.com',
+    databaseURL: 'https://transportes-villagomez.firebaseio.com',
+    projectId: 'transportes-villagomez',
+    storageBucket: '',
+    messagingSenderId: '676811609713',
+    appId: '1:676811609713:web:b9969b9e87bb4890'
+  }
 };
