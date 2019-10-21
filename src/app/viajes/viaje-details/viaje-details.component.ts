70f6f59288a7db2b41ca543be9a5c613b2e896c9
Rodrigo Bautista <rodbautista711@gmail.com>
2019-10-20 23:34:46 -0500

    slider for viaje terminado in viaje details.

--- src/app/viajes/viaje-details/viaje-details.component.ts
+++ src/app/viajes/viaje-details/viaje-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Carga, CargaProducto } from '../../business/model/carga.model';
+import { MatSlideToggleChange, MatDialog, MatSnackBar } from '@angular/material';
+import { ConfirmationDialogComponent } from 'src/app/shared/dialog/confirmation.dialog';
 
 @Component({
   selector: 'app-viaje-details',
@@ -7,33 +9,33 @@ import { Carga, CargaProducto } from '../../business/model/carga.model';
   styleUrls: ['./viaje-details.component.css']
 })
 export class ViajeDetailsComponent implements OnInit {
-
   @Input()
   truckName: string;
 
   @Input()
   viajes: Carga;
 
+  isViajeTerminado: boolean;
   destino: string;
   chofer: string;
   producto: string;
   ayudante: string;
   origen: string;
 
-  constructor() { }
+  constructor(private dialog: MatDialog, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
     this.findViajeDetailsByTruckName();
   }
 
   findViajeDetailsByTruckName() {
-    for (const load of this.viajes.cargasDetalles) {
-      if (load.camioneta === this.truckName) {
-        this.origen = load.nombreOrigen;
-        this.destino = load.nombreDestino;
-        this.chofer = load.chofer;
-        this.ayudante = load.ayudante;
-        this.producto = this.listProductos(load.productos);
+    for (const viaje of this.viajes.cargasDetalles) {
+      if (viaje.camioneta === this.truckName) {
+        this.origen = viaje.nombreOrigen;
+        this.destino = viaje.nombreDestino;
+        this.chofer = viaje.chofer;
+        this.ayudante = viaje.ayudante;
+        this.producto = this.listProductos(viaje.productos);
       }
     }
   }
@@ -46,4 +48,41 @@ export class ViajeDetailsComponent implements OnInit {
     return productosStr.substr(0, productosStr.length - 2);
   }
 
+  isViajeTerminadoChange(event: MatSlideToggleChange) {
+    this.isViajeTerminado = event.checked;
+    if (this.isViajeTerminado) {
+      this.openDialog(0);
+    }
+  }
+
+  openDialog(viajeIndex: number) {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        message: '¿Estas seguro que deseas terminar el viaje?',
+        buttonText: {
+          ok: 'Aceptar',
+          cancel: 'Cancelar'
+        }
+      }
+    });
+    const snack = this.snackBar;
+
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        this.terminarViaje(viajeIndex);
+        console.log('Actualizar viaje a terminado y liberar camioneta');
+        this.snackBar.open('Viaje terminado con éxito', 'Ok', {
+          duration: 2000
+        });
+      } else {
+        this.isViajeTerminado = false;
+      }
+    });
+  }
+  terminarViaje(viajeIndex: number) {
+    this.isViajeTerminado = true;
+    console.log('Viaje index: ' + viajeIndex);
+    console.log('Actualizar viaje a terminado y liberar camioneta');
+    // throw new Error('Method not implemented.');
+  }
 }
